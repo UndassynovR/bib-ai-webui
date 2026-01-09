184b667cd98b2bbb8c40782f42ee9be7ab3f0d78
Ramil <undassynovramil@gmail.com>
2026-01-09 13:34:43 +0500

    working

--- drizzle/migrations/20251210050231_fantastic_siren/migration.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX "sessions_token_idx" ON "sessions" ("token");--> statement-breakpoint
-CREATE INDEX "sessions_user_id_idx" ON "sessions" ("user_id");--> statement-breakpoint
-CREATE INDEX "users_email_idx" ON "users" ("email");--> statement-breakpoint
-ALTER TABLE "sessions" DROP COLUMN "created_at";
--- drizzle/migrations/20251212034910_young_vance_astro/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "theme" varchar(20) DEFAULT 'system' NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "language" varchar(10) DEFAULT 'auto' NOT NULL;
