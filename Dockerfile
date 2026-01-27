c9e8778443093f0043019b5af54fe16a0f488558
Ramil <undassynovramil@gmail.com>
2026-01-27 10:43:04 +0500

    APP_PORT setting

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,9 @@ RUN bun i
 
 COPY . .
 
-EXPOSE 5173
+ARG APP_PORT=5173
+ENV APP_PORT=${APP_PORT}
+EXPOSE ${APP_PORT}
 
 # CMD ["bun", "run", "dev", "--host", "0.0.0.0"]
 CMD ["sh", "-c", "bun run db:generate && bun run db:migrate && bun run dev --host 0.0.0.0"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [tailwindcss(), sveltekit()],
+    server: {
+      port: Number(env.APP_PORT ?? 5173),
+      host: true
+    }
+  };
 });
+
